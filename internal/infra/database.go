03e9aa140e44e2c2d619307e0ffefa8ff6aff7e6
MatheusPMatos <mpeima@yahoo.com.br>
2024-10-14 20:32:23 -0300

    ajuste database conection

--- internal/infra/database.go
+++ internal/infra/database.go
@@ -8,13 +8,16 @@ import (
 	"github.com/MatheusPMatos/api-aluga-quadras/internal/types"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func ConectaComBancodeDados(envs config.Environments) (*gorm.DB, error) {
 	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=require",
 		envs.DbHost, envs.DbUser, envs.DbPass, envs.DbName, envs.DbPort)
-	DB, err := gorm.Open(postgres.Open(dsn))
+	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Discard,
+	})
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
 		return nil, err
