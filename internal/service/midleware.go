c6b5d6cc8b94f55ea0c1752bdcac0a9c3a10471d
MatheusPMatos <mpeima@yahoo.com.br>
2024-10-16 20:17:34 -0300

    add atoi on midleware

--- internal/service/midleware.go
+++ internal/service/midleware.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/MatheusPMatos/api-aluga-quadras/internal/repository"
@@ -31,7 +32,13 @@ func (m *midleware) Auth() gin.HandlerFunc {
 			c.AbortWithError(http.StatusUnauthorized, err)
 			return
 		}
-		c.Set("user", id)
+
+		idUInt, err := strconv.Atoi(id)
+		if err != nil {
+			c.AbortWithError(http.StatusUnauthorized, err)
+			return
+		}
+		c.Set("user", idUInt)
 		c.Next()
 	}
 }
